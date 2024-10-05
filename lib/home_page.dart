3e52ca9758b9814e2eac0600ab468e3e8776ce27
Noor Mohammed Anik <noormohammedanik@gmail.com>
2024-10-05 16:59:47 +0600

    -updated

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,12 +8,17 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("GridView"),
+        backgroundColor: Colors.blueGrey,
+        title: Text(
+          "GridView",
+          style: TextStyle(color: Colors.white),
+        ),
       ),
       body: GridView.count(
         crossAxisCount: 3,
         mainAxisSpacing: 10,
         crossAxisSpacing: 10,
+        padding: EdgeInsets.all(10),
         children: [
           GestureDetector(
             onTap: () {
@@ -23,7 +28,10 @@ class HomePage extends StatelessWidget {
               elevation: 20,
               color: Colors.grey,
               child: Container(
-                color: Colors.green,
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(10)),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
@@ -44,164 +52,260 @@ class HomePage extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-            color: Colors.yellow,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.category,
-                  size: 50,
-                  color: Colors.white,
-                ),
-                Text(
-                  "Category",
-                  style: TextStyle(
+          GestureDetector(
+            onTap: () {
+              Fluttertoast.showToast(msg: "Category");
+            },
+            child: Card(
+              elevation: 20,
+              color: Colors.grey,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.category,
+                      size: 50,
                       color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Category",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
-          Container(
-            color: Colors.green,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.category_outlined,
-                  size: 50,
-                  color: Colors.white,
-                ),
-                Text(
-                  "Sub Category",
-                  style: TextStyle(
+          GestureDetector(
+            onTap: () {
+              Fluttertoast.showToast(msg: "Vendors");
+            },
+            child: Card(
+              elevation: 20,
+              color: Colors.grey,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.person_search_sharp,
+                      size: 50,
                       color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Vendors",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
-          Container(
-            color: Colors.yellow,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.production_quantity_limits,
-                  size: 50,
-                  color: Colors.white,
-                ),
-                Text(
-                  "Products",
-                  style: TextStyle(
+          GestureDetector(
+            onTap: () {
+              Fluttertoast.showToast(msg: "Products");
+            },
+            child: Card(
+              elevation: 20,
+              color: Colors.grey,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.shopping_cart,
+                      size: 50,
                       color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Products",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
-          Container(
-            color: Colors.green,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.add_shopping_cart,
-                  size: 50,
-                  color: Colors.white,
-                ),
-                Text(
-                  "Sales",
-                  style: TextStyle(
+          GestureDetector(
+            onTap: () {
+              Fluttertoast.showToast(msg: "Sales");
+            },
+            child: Card(
+              elevation: 20,
+              color: Colors.grey,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.note_alt_outlined,
+                      size: 50,
                       color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Sales",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
-          Container(
-            color: Colors.yellow,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.note_alt_outlined,
-                  size: 50,
-                  color: Colors.white,
-                ),
-                Text(
-                  "Orders",
-                  style: TextStyle(
+          GestureDetector(
+            onTap: () {
+              Fluttertoast.showToast(msg: "Orders");
+            },
+            child: Card(
+              elevation: 20,
+              color: Colors.grey,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.shopping_bag_outlined,
+                      size: 50,
                       color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Orders",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
-          Container(
-            color: Colors.green,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.account_box_outlined,
-                  size: 50,
-                  color: Colors.white,
-                ),
-                Text(
-                  "About",
-                  style: TextStyle(
+          GestureDetector(
+            onTap: () {
+              Fluttertoast.showToast(msg: "About");
+            },
+            child: Card(
+              elevation: 20,
+              color: Colors.grey,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.perm_contact_cal,
+                      size: 50,
                       color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "About",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
-          Container(
-            color: Colors.yellow,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.help,
-                  size: 50,
-                  color: Colors.white,
-                ),
-                Text(
-                  "Help",
-                  style: TextStyle(
+          GestureDetector(
+            onTap: () {
+              Fluttertoast.showToast(msg: "Help");
+            },
+            child: Card(
+              elevation: 20,
+              color: Colors.grey,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.help,
+                      size: 50,
                       color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Help",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
-          Container(
-            color: Colors.green,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.exit_to_app,
-                  size: 50,
-                  color: Colors.white,
-                ),
-                Text(
-                  "Logout",
-                  style: TextStyle(
+          GestureDetector(
+            onTap: () {
+              Fluttertoast.showToast(msg: "Logout");
+            },
+            child: Card(
+              elevation: 20,
+              color: Colors.grey,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.exit_to_app,
+                      size: 50,
                       color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Logout",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
         ],
